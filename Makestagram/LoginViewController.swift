2c5a2b434f39b5b5b157c2a344ca3e9a3c58a8f4
Isabelle <isabellemao@users.noreply.github.com>
2017-06-26 15:30:34 -0700

    Login
    This commit will add a login viewcontroller.
    As of this update, everything seems functional.

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Isabelle Mao on 2017-06-26.
+//  Copyright © 2017 Stupid Swifting. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+}
